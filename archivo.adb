764e622168d030c793ba9961a5dfedd75eba8b73
Laurenn Cruz <alecxandra.cruz@unitec.edu>
2016-03-02 16:58:06 -0600

    Gramatica while, loop y exit when

--- archivo.adb
+++ archivo.adb
@@ -1,12 +1,5 @@
- if x>6 then
-  put("en if");
-
- elsif z>= 15 then
-  if 6=8 then
-   put("funciona cosito");
-  end if;
-
- else 
-  put("en else");
- end if;
-
+ loop
+  loop
+   exit when c = true;
+  end loop;
+ end loop;
