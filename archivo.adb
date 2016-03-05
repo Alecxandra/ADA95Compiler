724a9c0e12414957e824dec8f86659bf088f48c3
Alecxandra <alecxandra.cruz@unitec.edu>
2016-03-04 19:57:56 -0600

    Gramatica de procedure con y sin parametros

--- archivo.adb
+++ archivo.adb
@@ -1,10 +1,28 @@
-procedure prueba(S3,S2: in out float) is
-   x,y,z : boolean;
- begin
-   procedure Media(x,y: in float;med: out float) is 
+--Ejemplo de subprogramas 
+
+procedure Ejemplo is --procedimiento principal Ejemplo 
+   
+  --función local EsPar 
+  function EsPar return boolean is 
+  begin 
+      return ((x / 2) = 0); 
+  end EsPar; 
+  --procedimiento local Media 
+  procedure Media(x,y: in float;med: out float) is 
       dos: float; --variable local 
   begin 
       dos := 2.0; 
       med := (x + y) / dos; 
   end Media;
-end prueba;
+
+  R1,R2,R3 : float; --variables locales de Ejemplo 
+  i : integer; 
+begin 
+    put("Entre un numero: "); 
+    get(i);
+         if EsPar(i) then put("es par"); else put("no es par"); end if; 
+    put("Entre una pareja de numeros separados por un espacio: "); 
+    get(R1); get(R2); 
+    Media(R1,R2,R3); 
+    Put(R3); 
+end Ejemplo; 
