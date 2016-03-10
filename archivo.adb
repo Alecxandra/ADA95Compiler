7e243b06165ee4865a604fc6b36b3f9561b2f587
Alecxandra <alecxandra.cruz@unitec.edu>
2016-03-10 10:15:27 -0600

    ultimos errores, ya no hare mas errores no no no

--- archivo.adb
+++ archivo.adb
@@ -1,8 +1,39 @@
-procedure Sum_Squares(A, B : in integer; Result : out integer) is
- function Square(X : in integer) return integer is
- begin -- this is the beginning of Square
- return X*X;
- end Square;
-begin -- this is the beginning of Sum_Squares
- Result := Square(A) +Square(B);
-end Sum_Squares; 
+procedure hola is
+  begin
+    put("hola mundo cruel");
+    --esto es un comentario
+    x: integer;
+    z,v,c : float;
+    
+    x:=0;
+    function y (x : in integer) return integer is
+     begin
+
+     end y;
+
+     procedure ex(x: in integer) is
+      begin
+
+      end ex;
+
+    if x>=10 then
+        x := 0;
+        put("hola hola hola");
+     elsif x=23 then
+      x:= 5**2;
+
+     elsif z/= 5.6 then
+        get (z);
+
+     else
+      
+     get(y);
+     y:= x- y;
+
+    end if;
+
+    while x<= 12.8 loop
+      put("hola amigos");
+    end loop;
+
+  end hola;
