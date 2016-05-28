1d23a8a4b7701270884b4e26226b72c1fb6d2a20
Carlos Banegas <carlos.banegas@outlook.com>
2016-05-27 21:08:20 -0600

    Intermediate Form, Expression and Statment started

---
+++ src/INTERM_LANG/IntermediateStatement.java
@@ -0,0 +1,115 @@
+package INTERM_LANG;
+
+import java.util.Vector;
+
+/**
+ *
+ * @author cbanegas
+ */
+public class IntermediateStatement extends IntermediateForm{
+    private Vector<Label> next;
+   
+    public IntermediateStatement() { 
+        this.next = new Vector();
+    }
+
+
+    public Vector<Label> getNext() {
+        return next;
+    }
+
+    public void setNext(Vector<Label> next) {
+        this.next = next;
+    }
+    
+     public String buildIntermediateCode() {
+        StringBuilder sb = new StringBuilder();
+          
+        for (int i = 0; i < operations.size(); i++) {
+            Quadruple currentQuadruple = operations.elementAt(i);
+            switch(currentQuadruple.getType()) {
+                case ADD: {
+                    sb.append(currentQuadruple.getStore() + " := " + currentQuadruple.getOp1() + " + " + currentQuadruple.getOp2());
+                    break;
+                }
+                case MIN: {
+                    sb.append(currentQuadruple.getStore() + " := " + currentQuadruple.getOp1() + " - " + currentQuadruple.getOp2());
+                    break;
+                }
+                case UMIN: {
+                    sb.append(currentQuadruple.getStore() + " := -" + currentQuadruple.getOp1());
+                    break;
+                }
+                case MUL: {
+                    sb.append(currentQuadruple.getStore() + " := " + currentQuadruple.getOp1() + " * " + currentQuadruple.getOp2());
+                    break;
+                }
+                case DIV: {
+                    sb.append(currentQuadruple.getStore() + " := " + currentQuadruple.getOp1() + " / " + currentQuadruple.getOp2());
+                    break;
+                }
+                case IF_GEQ: {
+                    sb.append("if " + currentQuadruple.getOp1() + " >= " + currentQuadruple.getOp2() + " goto " + currentQuadruple.getLabel());
+                    break;
+                }
+                case IF_LEQ: {
+                    sb.append("if " + currentQuadruple.getOp1() + " <= " + currentQuadruple.getOp2() + " goto " + currentQuadruple.getLabel());
+                    break;
+                }
+                case IF_GT: {
+                    sb.append("if " + currentQuadruple.getOp1() + " > " + currentQuadruple.getOp2() + " goto " + currentQuadruple.getLabel());
+                    break;
+                }
+                case IF_LT: {
+                    sb.append("if " + currentQuadruple.getOp1() + " < " + currentQuadruple.getOp2() + " goto " + currentQuadruple.getLabel());
+                    break;
+                }
+                case IF_NEQ: {
+                    sb.append("if " + currentQuadruple.getOp1() + " <> " + currentQuadruple.getOp2() + " goto " + currentQuadruple.getLabel());
+                    break;
+                }
+                case IF_EQ: {
+                    sb.append("if " + currentQuadruple.getOp1() + " = " + currentQuadruple.getOp2() + " goto " + currentQuadruple.getLabel());
+                    break;
+                }
+                case ASSIGN: {
+                    sb.append(currentQuadruple.getStore() + " := " + currentQuadruple.getOp1());
+                    break;
+                }
+                case PARAM: {
+                    sb.append("params " + currentQuadruple.getOp1());
+                    break;
+                }
+                case CALL: {
+                    sb.append("call " + currentQuadruple.getOp1() + ", " + currentQuadruple.getOp2());
+                    break;
+                }
+                case GOTO: {
+                    sb.append("goto " + currentQuadruple.getLabel());
+                    break;
+                }
+                case PRINT: {
+                    sb.append("print " + currentQuadruple.getOp1());
+                    break;
+                }
+                case READ: {
+                    sb.append("read " + currentQuadruple.getOp1());
+                    break;
+                }
+                case LABEL: {
+                    sb.append(currentQuadruple.getLabel() + ":");
+                    break;
+                }
+                case EXIT: {
+                    sb.append(currentQuadruple.getOp1());
+                    break;
+                }
+                case VOID_RET: {
+                    sb.append(currentQuadruple.getOp1());
+                    break;
+                }
+            }
+        }
+        return sb.toString();
+     }
+}
