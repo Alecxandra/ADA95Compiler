c2792f2f49f21dfd88488449719bd74d495aa75a
Alecxandra <alecxandra.cruz@unitec.edu>
2016-05-28 18:06:41 -0600

    archivo adb

--- archivo.adb
+++ archivo.adb
@@ -1,31 +1,9 @@
 procedure principal is
-    x,y,z : integer;
-    h:float;
+x,y: integer;
 
-    procedure una_prueba(a,b: in integer; c: out integer) is
-        algo1 : integer;
-        begin
-                    c := a + b;
-                    if c = 10 then
-                        put(x);
-                    else
-                        c := 20;
-                    end if;
-    end una_prueba;
-
-    function algo (dinero : in integer; algo1: in out float ) return float is
-            total : integer;
-            begin
-     
-            while total >= 0 loop
-                get(total);
-            end loop;
-        end algo;
-    
-    begin 
-            exit when 10>3;
-            x := 10;
-            una_prueba(x,10,z);
-            h := algo(12,34.0);
-            put(h);
+begin
+    x := 0.0
+    if x > 10 then
+     put("Hola mundo cruel");
+    end if;
 end principal;
